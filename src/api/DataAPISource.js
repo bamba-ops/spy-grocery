854b141189016a2124e69f3711b5ef63f76093c6
wgharbi <wgharbi@dept-info.crosemont.quebec>
2025-01-11 13:19:34 -0500

    changing the api url

--- src/api/DataAPISource.js
+++ src/api/DataAPISource.js
@@ -4,7 +4,7 @@ import { Product } from '@/domain/entity/Product';
 import { Price } from '@/domain/entity/Price';
 
 
-const API_URL = 'spy-grocery-backend-production.up.railway.app/api/v1';
+const API_URL = 'https://spy-grocery-backend-production.up.railway.app/api/v1';
 const STORE_ID = '32d6dd89-4216-4588-a096-631bfaf5df56'
 const GET_ALL_PRICES_BY_STORE_ID = `/prices/store/${STORE_ID}`
 const GET_BEST_PRICE = `/product/price`
