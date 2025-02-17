9199e245ff7dfc04042f6edd50d5675025c9dffc
wgharbi <wgharbi@dept-info.crosemont.quebec>
2025-02-16 20:22:38 -0500

    new version coming

--- src/api/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://suwbktyfyzqyhsdidylx.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN1d2JrdHlmeXpxeWhzZGlkeWx4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA3Njc4NDYsImV4cCI6MjA0NjM0Mzg0Nn0.p4awQmeUHRqzUY5j1E81VUASbYlqSS8p2I9Cm3BPlwA'
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
